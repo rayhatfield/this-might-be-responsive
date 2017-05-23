b0b2f636c143672f57fd52c4ebc22b9ce3fb9e4c
Ray Hatfield <ray.hatfield@nextthought.com>
2017-05-22 22:44:33 -0500

    don't show nav if you're not logged in

--- src/main/js/app/components/Header.jsx
+++ src/main/js/app/components/Header.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {NavLink as Link} from 'react-router-dom';
 
-export default function Header () {
+Header.propTypes = {
+	userid: PropTypes.string
+};
+
+function Header ({userid}) {
 	return (
 		<header className="app-header">
 			<h1>[ this might be offensive ]</h1>
-			<nav>
-				<Link to="/images">images</Link>
-				<Link to="/discussions">discussions</Link>
-			</nav>
+			{
+				userid &&
+				(
+					<nav>
+						<Link to="/images">images</Link>
+						<Link to="/discussions">discussions</Link>
+					</nav>
+				)
+			}
 		</header>
 	);
 }
+
+const mapStateToProps = ({login: {userid}}) => ({
+	userid
+});
+
+import {connect} from 'react-redux';
+export default connect(mapStateToProps)(Header);
