ff5e18611544047f75c9c6a152fbfdb0ae5e4e2d
Ray Hatfield <ray.hatfield@nextthought.com>
2017-05-23 11:54:40 -0500

    use animated svg for loading indicator

--- src/main/js/common/components/Loading.jsx
+++ src/main/js/common/components/Loading.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import ELLIPSIS from '../assets/animated-ellipsis.svg';
+
 export default function Loading () {
 	return (
-		<div className="loading">[ loading ]</div>
+		<div className="loading"><img src={ELLIPSIS} /></div>
 	);
 }
